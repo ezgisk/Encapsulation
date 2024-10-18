f4eb250c4f321b39aa95d3e6297bba581df29571
Gorkem <Gorkem@DESKTOP-2DV200H>
2024-10-17 20:04:11 -0700

    Add project files.

---
+++ Encapsulation/Araba.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Araba // Creating Araba class
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+        private int _kapiSayisi; // Encapsulation
+
+        public int KapiSayisi
+        {
+            get
+            {
+                return _kapiSayisi;
+            }
+            set
+            {
+                if (value == 2 && value == 4)// Kapi sayisi iki ya da dort ise  dogru degeri donuyor
+                {
+                    _kapiSayisi = value;
+                } 
+                else
+                {
+                    Console.WriteLine("Hatalı kapı sayısı! Kapı sayısı 2 veya 4 olmalıdır. Kapı sayısı -1 olarak atanıyor.");
+                    value = -1;
+                }
+            }
+        }
+        public Araba(string marka, string model, string renk, int kapiSayisi) // Araba constructor
+        {
+            Marka = marka;
+            Model = model;
+            Renk = renk;    
+            KapiSayisi = kapiSayisi;
+
+
+        }
+    }
+
+    
+}
+
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,6 @@
+﻿using Encapsulation;
+
+Araba araba = new Araba("Toyota","Rav4","Mavi",4);
+Console.WriteLine($"Marka: {araba.Marka}, Model: {araba.Model}, Renk: {araba.Renk}, Kapı Sayısı: {araba.KapiSayisi}");
+
+Console.ReadKey();
